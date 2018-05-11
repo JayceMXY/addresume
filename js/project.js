43aad9a720abc88eb4080ee3147db5311eb7a3d5
chengruilin <chengruilin@codemao.cn>
2018-05-12 00:04:49 +0800

    使用$需要先引入jquery、projuect.js里面语法报错

--- js/project.js
+++ js/project.js
@@ -2,5 +2,5 @@ $(`.project .tab li`).click(function(evevt){
     let $index = $(this).index()
     $(this).addClass('active').siblings().removeClass('active')
     
-    $(`.project .content li`.removeClass('active').eq($index).addClass('active'))
+    $(`.project .content li`).removeClass('active').eq($index).addClass('active')
 })
